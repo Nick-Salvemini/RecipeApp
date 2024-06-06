941ddab1128e754923a59394ac18d60aaa019c66
Nick Salvemini <salvemini.nick@gmail.com>
2024-06-06 16:25:57 -0400

    fixed routing

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,51 +1,19 @@
 import React from 'react';
 import { AuthProvider } from './hooks/AuthContext';
 import AppRoutes from './AppRoutes';
-import GlobalErrorHandler from './components/errors/GlobalErrorHandler';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Common/Navbar';
-import {
-  createBrowserRouter, createRoutesFromElements,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-import LoginForm from './components/Auth/LoginForm';
-import RegisterForm from './components/Auth/RegisterForm';
-
-// console.log('Running on port:', process.env.PORT);
 
 function App() {
   console.log('Rendering App component');
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<LoginForm />} >
-        <Route path="/register" element={<RegisterForm />} />
-
-      </Route>
-    ))
-
-
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <div className="App">
+        <AppRoutes />
+      </div>
     </AuthProvider>
-  )
-  /*
-  return (
-    <Router>
-      
-        <GlobalErrorHandler>
-          <div className="App">
-            <AppRoutes />
-          </div>
-        </GlobalErrorHandler>
-      </AuthProvider>
-    </Router>
   );
-  */
 }
 
 export default App;
--- frontend-vite/src/AppRoutes.jsx
+++ frontend-vite/src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from 'react-router-dom';
 import LoginForm from './components/Auth/LoginForm';
 import RegisterForm from './components/Auth/RegisterForm';
 import RecipeList from './components/Recipes/RecipeList';
@@ -8,22 +8,34 @@ import RecipeForm from './components/Recipes/RecipeForm';
 import PrivateRoute from './components/Common/PrivateRoute';
 import Navbar from './components/Common/Navbar';
 import Footer from './components/Common/Footer';
+import GlobalErrorHandler from './components/errors/GlobalErrorHandler';
+
+const Layout = () => (
+    <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+        <GlobalErrorHandler />
+    </>
+);
+
+const routes = createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+        <Route path="login" element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+        <Route path="/" element={<PrivateRoute><RecipeForm /></PrivateRoute>} />
+        <Route path="recipes" element={<PrivateRoute><RecipeList /></PrivateRoute>} />
+        <Route path="recipe/:id" element={<PrivateRoute><RecipeDetails /></PrivateRoute>} />
+        <Route path="new-recipe" element={<PrivateRoute><RecipeForm /></PrivateRoute>} />
+    </Route>
+);
+
+const router = createBrowserRouter(routes);
 
 const AppRoutes = () => {
     console.log('Rendering routes in AppRoutes');
     return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/register" element={<RegisterForm />} />
-                <Route path="/" element={<PrivateRoute><RecipeForm /></PrivateRoute>} />
-                <Route path="/recipes" element={<PrivateRoute><RecipeList /></PrivateRoute>} />
-                <Route path="/recipe/:id" element={<PrivateRoute><RecipeDetails /></PrivateRoute>} />
-                <Route path="/new-recipe" element={<PrivateRoute><RecipeForm /></PrivateRoute>} />
-            </Routes>
-            <Footer />
-        </>
+        <RouterProvider router={router} />
     );
 };
 
