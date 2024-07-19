e7049a5999d25717b26d84135acddea5e28d0721
Nick Salvemini <salvemini.nick@gmail.com>
2024-07-19 14:07:24 -0400

    updated

--- frontend-vite/src/utilities/config.jsx
+++ frontend-vite/src/utilities/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+    apiBaseUrl: process.env.VITE_API_BASE_URL || "http://localhost:3000",
 };
 
 console.log("Configured API Base URL:", config.apiBaseUrl);
